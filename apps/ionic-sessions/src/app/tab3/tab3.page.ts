4ea7d80425aa9f9e3e2ceddc2f626e415c1af051
Nikola Rusakov <rusakov.nikola@gmail.com>
2022-10-18 23:41:40 +0200

    feat: generate ionic project

---
+++ apps/ionic-sessions/src/app/tab3/tab3.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sessions-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss'],
+})
+export class Tab3Page {
+  constructor() {}
+}
---
+++ apps/ionic-sessions/src/app/tabs/tabs.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sessions-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss'],
+})
+export class TabsPage {
+  constructor() {}
+}
