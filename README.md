0044e6c4ab320eaec714d60534bab50217fcfa20
Nikola Rusakov <rusakov.nikola@gmail.com>
2023-03-31 19:22:46 +0200

    feat(analog): introduce analog + copy nx generator of analog

--- README.md
+++ README.md
@@ -6,3 +6,35 @@ KOTLIN --buildSystem=GRADLE_KOTLIN --test=KOTEST --features=openapi,swagger-ui,l
 ```
 type=DEFAULT&name=sessions&package=doto.solutions&javaVersion=JDK_17&lang=KOTLIN&build=GRADLE_KOTLIN&test=KOTEST --features=openapi,swagger-ui,liquibase,microstream,multi-tenancy,postgres,microstream-rest,agorapulse-gru-http,tracing-opentelemetry-jaeger,kotlin-extension-functions,micrometer-prometheus,nats,graalvm,crac,micronaut-aot,reactor,netflix-hystrix,security-oauth2,security-jwt,discovery-consul,problem-json,views-velocity,config4k,test-resources,netty-server,graphql,redis-lettuce,testcontainers,jackson-databind,serialization-jackson&version=3.7.1
 ```
+
+## tools
+Nx
+Storybook
+Playwright
+
+## fe
+tailwindcss / windicss - react
+analog.js
+angular
+astro
+state-adapt; ngneat/elf
+
+## be 
+terminusdb - graph db
+h3 be + zod - http server
+drizzle-orm - orm
+
+## OPS
+SST - deployment
+Dagger.io - CI/CD
+
+## API
+graphql client on BE
+meilisearch client - full-text search
+orval - api client
+Casdoor - IAM,SSO,OIDC, permissions
+MinIO
+
+## Data
+cube.js - user analytics
+jitsu - data integration platform
---
+++ apps/analog-app/vite.config.ts
@@ -0,0 +1,64 @@
+/// <reference types="vitest" />
+
+import analog from '@analogjs/platform';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig, Plugin, splitVendorChunkPlugin } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    publicDir: 'src/public',
+    optimizeDeps: {
+      include: ['@angular/common', '@angular/forms'],
+    },
+    build: {
+      target: ['es2020'],
+    },
+    plugins: [
+      analog({
+        ssrBuildDir: '../../dist/apps/analog-app/ssr',
+        entryServer: 'apps/analog-app/src/main.server.ts',
+        vite: {
+          inlineStylesExtension: 'css',
+          tsconfig:
+            mode === 'test'
+              ? 'apps/analog-app/tsconfig.spec.json'
+              : 'apps/analog-app/tsconfig.app.json',
+        },
+        nitro: {
+          rootDir: 'apps/analog-app',
+          output: {
+            dir: '../../../dist/apps/analog-app/analog',
+            publicDir: '../../../dist/apps/analog-app/analog/public',
+          },
+          publicAssets: [{ dir: `../../../dist/apps/analog-app/client` }],
+          serverAssets: [
+            { baseName: 'public', dir: `./dist/apps/analog-app/client` },
+          ],
+          buildDir: '../../dist/apps/analog-app/.nitro',
+          prerender: {
+            routes: ['/'],
+          },
+        },
+      }),
+      tsConfigPaths({
+        root: '../../',
+      }),
+      visualizer() as Plugin,
+      splitVendorChunkPlugin(),
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test-setup.ts'],
+      include: ['**/*.spec.ts'],
+      cache: {
+        dir: `../../node_modules/.vitest`,
+      },
+    },
+    define: {
+      'import.meta.vitest': mode !== 'production',
+    },
+  };
+});
