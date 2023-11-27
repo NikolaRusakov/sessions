7469a31a20b8708ad264220eb15293f68b824799
Nikola Rusakov <rusakov.nikola@gmail.com>
2023-11-27 18:01:15 +1000

    feat: session source pocs

--- apps/pre-form/src/main.tsx
+++ apps/pre-form/src/main.tsx
@@ -1,16 +1,6 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
+import { render } from 'preact';
 import App from './app/app';
+import './styles.css';
+import 'virtual:uno.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+render(<App />, document.getElementById('root'));
--- apps/pre-form/vite.config.ts
+++ apps/pre-form/vite.config.ts
@@ -1,43 +1,44 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
+// import react from "@vitejs/plugin-react";
+import preact from '@preact/preset-vite';
+import prefresh from '@prefresh/vite';
 
+import alias from '@rollup/plugin-alias';
+import UnoCSS from 'unocss/vite';
+import presetIcons from '@unocss/preset-icons';
+import presetUno from '@unocss/preset-uno';
+import presetAttributify from '@unocss/preset-attributify';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/pre-form',
-
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
   plugins: [
-    react(),
-    viteTsConfigPaths({
-      root: '../../',
+    // prefresh(),
+    preact(),
+    UnoCSS({
+      shortcuts: [
+        {
+          logo: 'i-logos-react w-6em h-6em transform transition-800 hover:rotate-180',
+        },
+      ],
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons({
+          extraProperties: {
+            display: 'inline-block',
+            'vertical-align': 'middle',
+          },
+        }),
+      ],
+      inspector: true,
+    }),
+    alias({
+      entries: [
+        { find: 'react', replacement: 'preact/compat' },
+        { find: 'react-dom/test-utils', replacement: 'preact/test-utils' },
+        { find: 'react-dom', replacement: 'preact/compat' },
+        { find: 'react/jsx-runtime', replacement: 'preact/jsx-runtime' },
+      ],
     }),
   ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: '../../',
-  //    }),
-  //  ],
-  // },
-
-  test: {
-    globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
 });
